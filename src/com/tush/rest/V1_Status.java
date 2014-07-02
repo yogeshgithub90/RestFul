b860c339cfdd650cfc2dff6323ffc26d3c6abebc
tushar8590 <tushar8590@gmail.com>
2014-07-02 23:02:23 +0530

    finished of ep2 of tutorial

---
+++ src/com/tush/rest/V1_Status.java
@@ -0,0 +1,14 @@
+package com.tush.rest;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+
+@Path("/v1/status")
+public class V1_Status {
+
+	@GET
+	@Produces(MediaType.TEXT_HTML)
+	public String returnTitle(){
+		return "<p> Java Web Services </p>";
+	}
+}
